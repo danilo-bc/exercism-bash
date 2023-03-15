5cdb0208f424a3c1c607cb0dc1dde294d652e81c
Danilo Cavalcanti <danilobcaval@gmail.com>
2023-03-15 12:28:48 -0300

    Solution to armstrong-numbers problem

--- armstrong-numbers/armstrong_numbers.sh
+++ armstrong-numbers/armstrong_numbers.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+main () {
+    calculation=0
+
+    for (( i = 0; i < ${#1}; ++i )); do
+        calculation=$(( $calculation+ ${1:$i:1}**(${#1}) ))
+    done
+    if [[ $1 == $calculation ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
+main "$@"
