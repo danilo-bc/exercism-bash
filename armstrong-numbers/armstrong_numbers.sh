afeb586f066a413e1515f88cb364d44615b33422
Danilo Cavalcanti <danilobcaval@gmail.com>
2023-03-15 12:33:06 -0300

    Updated armstrong-numbers solution with shellcheck suggestions

--- armstrong-numbers/armstrong_numbers.sh
+++ armstrong-numbers/armstrong_numbers.sh
@@ -4,9 +4,9 @@ main () {
     calculation=0
 
     for (( i = 0; i < ${#1}; ++i )); do
-        calculation=$(( $calculation+ ${1:$i:1}**(${#1}) ))
+        calculation=$(( calculation+ ${1:i:1}**(${#1}) ))
     done
-    if [[ $1 == $calculation ]]; then
+    if [[ $1 == "$calculation" ]]; then
         echo "true"
     else
         echo "false"
